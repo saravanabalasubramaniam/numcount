58313e55634b9e52daf406fe9163234cf458f584
saravana <saravanasaru14313@gmail.com>
2019-02-06 02:42:46 -0800

    Update README.java

--- README.java
+++ README.java
@@ -5,7 +5,6 @@ class Numcount
 public static void main(String args[])
 	{
 	Scanner input=new Scanner(System.in);
-	System.out.println("Enter the input");
 	String a=input.nextLine();
 	int count=0;
 	for(int i=0;i<a.length();i++)
